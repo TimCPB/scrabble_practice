6a2d22385e3e16ae2a8f7479eb93847c1095781a
Tim <TimCPB@github.com>
2020-08-10 18:20:36 +0100

    passing score method returns 6 for the word 'street'

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,10 +1,36 @@
 class Scrabble
 
   def initialize(word)
+    @word = word.split('')
+    @one = ['a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't']
+    @two = ['d', 'g']
+    @three = ['b', 'c', 'm', 'p']
+    @four = ['f', 'h', 'v', 'w', 'y']
+    @five = ['k']
+    @eight = ['j', 'x']
+    @ten = ['q', 'z']
+    @score = 0
   end
 
   def score
-    0
+      @word.each do |letter|
+        if @one.include?(letter)
+          @score += 1
+        elsif @two.include?(letter)
+          @score += 2
+        elsif @three.include?(letter)
+          @score += 3
+        elsif @four.include?(letter)
+          @score += 4
+        elsif @five.include?(letter)
+          @score += 5
+        elsif @eight.include?(letter)
+          @score += 8
+        elsif @ten.include?(letter)
+          @score += 10
+        end
+      end
+      @score
   end
 
 end
--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -5,4 +5,17 @@ describe Scrabble do
     scrabble = Scrabble.new('')
     expect(scrabble.score).to eq(0)
   end
+
+  it 'score method returns 1 for the letter a' do
+    scrabble = Scrabble.new('a')
+    expect(scrabble.score).to eq(1)
+  end
+   it 'score method returns 4 for the letter f' do
+    scrabble = Scrabble.new('f')
+    expect(scrabble.score).to eq(4)
+   end
+   it "score method returns 6 for the word 'street'" do
+    scrabble = Scrabble.new('street')
+    expect(scrabble.score).to eq(6)
+   end
 end
